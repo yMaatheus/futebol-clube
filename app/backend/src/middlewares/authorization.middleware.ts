bc531b6b0a4b7fbd3b1bb56d2839c474546f1e85
Matheus <stvgamer74@gmail.com>
2022-08-24 19:08:17 -0300

    feat: :white_check_mark: create middleware authorization token and add route login validate

---
+++ app/backend/src/middlewares/authorization.middleware.ts
@@ -0,0 +1,16 @@
+import { NextFunction, Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { verifyJwtToken } from '../utils/jwt.util';
+import CustomError from '../utils/customError.util';
+
+export default (req: Request, res: Response, next: NextFunction) => {
+  const { authorization: token } = req.headers;
+
+  if (!token) throw new CustomError(StatusCodes.UNAUTHORIZED, 'Authorization code not informed');
+
+  const { user } = verifyJwtToken(token);
+
+  res.locals.user = user;
+
+  next();
+};
--- app/backend/src/routes/login.route.ts
+++ app/backend/src/routes/login.route.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import authorization from '../middlewares/authorization.middleware';
 import LoginController from '../controllers/login.controller';
 
 const route = Router();
@@ -6,5 +7,6 @@ const route = Router();
 const loginController = new LoginController();
 
 route.post('/', loginController.authUser);
+route.get('/validate', authorization);
 
 export default route;
