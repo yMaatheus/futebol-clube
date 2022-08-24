de89ca622d5994bcb16c2e76a3140955640651ab
Matheus <stvgamer74@gmail.com>
2022-08-24 02:16:51 -0300

    feat: :sparkles: create models

---
+++ app/backend/src/database/models/match.ts
@@ -0,0 +1,52 @@
+import { BOOLEAN, INTEGER, Model } from 'sequelize';
+import db from '.';
+import Team from './team';
+
+export default class Match extends Model {
+  public id!: number;
+  public homeTeam!: number;
+  public homeTeamGoals!: number;
+  public awayTeam!: number;
+  public awayTeamGoals!: number;
+  public inProgress!: boolean;
+}
+
+Match.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  homeTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+});
+
+Match.belongsTo(Team, { foreignKey: 'homeTeam', as: 'home' });
+Match.belongsTo(Team, { foreignKey: 'awayTeam', as: 'away' });
+
+Team.hasMany(Match, { foreignKey: 'homeTeam', as: 'matchHomeTeam' });
+Team.hasMany(Match, { foreignKey: 'awayTeam', as: 'matchAwayTeam' });
---
+++ app/backend/src/database/models/team.ts
@@ -0,0 +1,25 @@
+import { STRING, INTEGER, Model } from 'sequelize';
+import db from '.';
+
+export default class Team extends Model {
+  public id!: number;
+  public teamName!: string;
+}
+
+Team.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+});
