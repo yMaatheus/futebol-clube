f0fc69831dcb81a8f1e9557e3cbe81666ee84d75
Matheus <stvgamer74@gmail.com>
2022-08-24 16:27:09 -0300

    test: :test_tube: add tests from not inform arguments in route login

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -14,12 +14,34 @@ const { expect } = chai;
 describe('Login', () => {
   let chaiHttpResponse: Response;
 
-  it('Returns status 200 and body contain token property', async () => {
-    chaiHttpResponse = await chai.request(app)
-    .post('/login')
-    .send({ email: 'admin@admin.com', password: 'secret_admin'})
-
-    expect(chaiHttpResponse.status).to.equal(200);
-    expect(chaiHttpResponse.body).to.have.property('token');
-  });
+  describe('Route /login', () => {
+    it('Returns status 200 and body contain token property', async () => {
+      chaiHttpResponse = await chai.request(app)
+      .post('/login')
+      .send({ email: 'admin@admin.com', password: 'secret_admin'})
+  
+      expect(chaiHttpResponse.status).to.equal(200);
+      expect(chaiHttpResponse.body).to.have.property('token');
+    });
+  
+    it('If "email" is not informed returns status 400 and message "All fields must be filled"', async () => {
+      chaiHttpResponse = await chai.request(app)
+      .post('/login')
+      .send({ password: 'secret_admin'})
+  
+      expect(chaiHttpResponse.status).to.equal(400);
+      expect(chaiHttpResponse.body).to.have.property('message');
+      expect(chaiHttpResponse.body.message).to.be.equal('All fields must be filled');
+    });
+  
+    it('If "password" is not informed returns status 400 and message "All fields must be filled"', async () => {
+      chaiHttpResponse = await chai.request(app)
+      .post('/login')
+      .send({ email: 'admin@admin.com' })
+  
+      expect(chaiHttpResponse.status).to.equal(400);
+      expect(chaiHttpResponse.body).to.have.property('message');
+      expect(chaiHttpResponse.body.message).to.be.equal('All fields must be filled');
+    });
+  })
 });
