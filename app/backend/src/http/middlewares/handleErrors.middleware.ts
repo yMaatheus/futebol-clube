13872c6c7da6115184c77a8b25d7de6bd27d994c
Matheus <stvgamer74@gmail.com>
2022-08-25 18:15:54 -0300

    fix: :bug: resolve bug that crash app in error middleware

--- app/backend/src/http/middlewares/handleErrors.middleware.ts
+++ app/backend/src/http/middlewares/handleErrors.middleware.ts
@@ -1,14 +1,18 @@
 import { ErrorRequestHandler, Request, Response, NextFunction } from 'express';
 import { getStatusCode } from 'http-status-codes';
 
+const getCode = (name: string): number => {
+  try {
+    return getStatusCode(name);
+  } catch (error) {
+    return 500;
+  }
+};
+
 const error: ErrorRequestHandler = async (err, _r: Request, res: Response, _next: NextFunction) => {
   const { name, message } = err;
 
-  const code = getStatusCode(name);
-
-  if (!code) return res.status(500).json({ message });
-
-  return res.status(+code).json({ message });
+  return res.status(getCode(name)).json({ message });
 };
 
 export default error;
