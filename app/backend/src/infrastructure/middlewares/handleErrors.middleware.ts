20e03a7964ceb822c381fc97764ecfdd6b0601ea
Matheus <stvgamer74@gmail.com>
2022-10-07 04:45:35 -0300

    refactor: :recycle: refactoring handle errors middleware

--- app/backend/src/infrastructure/middlewares/handleErrors.middleware.ts
+++ app/backend/src/infrastructure/middlewares/handleErrors.middleware.ts
@@ -1,18 +1,17 @@
 import { ErrorRequestHandler, Request, Response, NextFunction } from 'express';
 import { getStatusCode } from 'http-status-codes';
 
-const getCode = (name: string): number => {
-  try {
-    return getStatusCode(name);
-  } catch (error) {
-    return 500;
-  }
-};
-
 const error: ErrorRequestHandler = async (err, _r: Request, res: Response, _next: NextFunction) => {
   const { name, message } = err;
+  let code;
 
-  return res.status(getCode(name)).json({ message });
+  try {
+    code = getStatusCode(name);
+  } catch (_error) {
+    code = 500;
+  }
+
+  return res.status(code).json({ message });
 };
 
 export default error;
