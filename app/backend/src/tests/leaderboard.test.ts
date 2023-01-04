e4b5e8c5b1f82c2441488ee023ad57f36b232d15
Matheus <stvgamer74@gmail.com>
2023-01-03 23:34:44 -0300

    test: :test_tube: refactor test to route home leaderboard and add test to route away leaderboard

--- app/backend/src/tests/leaderboard.test.ts
+++ app/backend/src/tests/leaderboard.test.ts
@@ -7,7 +7,7 @@ import chaiHttp = require('chai-http');
 import { app } from '../app';
 
 import { Response } from 'superagent';
-import { leaderboardDatabase } from './utils/leaderboardData';
+import { leaderboardAwayDatabase, leaderboardHomeDatabase } from './utils/leaderboardData';
 
 chai.use(chaiHttp);
 
@@ -19,12 +19,22 @@ describe('Leaderboard', () => {
   afterEach(() => sinon.restore());
 
   describe('Route GET /leaderboard/home', () => {
-    it('Returns status 200 and leaderboard', async () => {
+    it('Returns status 200 and home leaderboard', async () => {
       chaiHttpResponse = await chai.request(app)
         .get('/leaderboard/home');
 
       expect(chaiHttpResponse.status).to.equal(200);
-      expect(chaiHttpResponse.body).to.deep.equal(leaderboardDatabase);
+      expect(chaiHttpResponse.body).to.deep.equal(leaderboardHomeDatabase);
+    })
+  })
+
+  describe('Route GET /leaderboard/away', () => {
+    it('Returns status 200 and away leaderboard', async () => {
+      chaiHttpResponse = await chai.request(app)
+        .get('/leaderboard/away');
+
+      expect(chaiHttpResponse.status).to.equal(200);
+      expect(chaiHttpResponse.body).to.deep.equal(leaderboardAwayDatabase);
     })
   })
 })
